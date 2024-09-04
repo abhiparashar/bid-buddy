379faff38e05318dc47294d3c0317f94df35c53d
Abhishek Parashar <abhishek.parashar@nium.com>
2024-09-04 11:31:36 +0530

    drizzle adaptor done

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,10 @@
 import NextAuth from "next-auth";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import GitHubProvider from "next-auth/providers/github";
-import { Adapter } from "next-auth/adapters";
+import { database } from "@/app/db/database";
 
 const handler = NextAuth({
+  adapter: DrizzleAdapter(database),
   providers: [
     GitHubProvider({
       clientId: "Ov23liyUpezlkmuiMAZf",
--- app/env.ts
+++ app/env.ts
@@ -6,6 +6,8 @@ export const env = createEnv({
     NODE_ENV: z.string().min(1),
     GITHUB_ID: z.string().min(1),
     GITHUB_SECRET: z.string().min(1),
+    AUTH_DRIZZLE_URL: z.string().min(1),
+    AUTH_SECRET: z.string().min(1),
   },
   client: {},
   runtimeEnv: {
@@ -15,5 +17,7 @@ export const env = createEnv({
       "postgres://postgres:password@localhost:5433/mydatabase",
     GITHUB_ID: "Ov23liyUpezlkmuiMAZf",
     GITHUB_SECRET: "e90a9110deb760a5ea6c5326548eb9defcb7822d",
+    AUTH_DRIZZLE_URL: "postgres://postgres:password@localhost:5433/mydatabase",
+    AUTH_SECRET: "LdyJhBTDMIudVu6ZzmBHJij8nsortrc0JZuS7BUtXfY=",
   },
 });
